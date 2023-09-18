1ef2ba60dbe836df21b849b72c4b6925ae118704
Ernest Prabhakar <ernest@quiltdata.io>
2023-09-18 15:56:58 -0700

    test_agify_gender

--- udc-py/tests/test_udc.py
+++ udc-py/tests/test_udc.py
@@ -1,5 +1,6 @@
 from click.testing import CliRunner
 from udc_py import hello
+from pyagify.agify import GenderizeClient
 
 
 def test_hello_world():
@@ -7,3 +8,8 @@ def test_hello_world():
     result = runner.invoke(hello, ["Peter"])
     assert result.exit_code == 0
     assert result.output == "Hello Peter!\n"
+
+def test_agify_gender():
+    client = GenderizeClient()
+    gender = client.get_gender("Allen")
+    assert gender == "male"
