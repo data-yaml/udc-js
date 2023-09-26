6ef2402d5f90ec50a009500e8602d4d7747857ed
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2023-09-26 11:50:31 -0700

    Udc java (#10)
    * gradele init
    
    * build rhino
    
    * print Rhino result
    
    * add tests
    
    * rename Java Test
    
    * ./gradlew build test
    
    * lint yaml
    
    * more linting

---
+++ udc-java/app/src/main/groovy/udc/java/Rhino.groovy
@@ -0,0 +1,34 @@
+// Import and run a Rhino script to do "hello world" in Java.
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+import groovy.transform.CompileDynamic;
+
+@CompileDynamic
+class Rhino {
+
+    public static String run(String script) {
+        Rhino rhino = new Rhino();
+        return rhino.execute(script);
+    }
+
+    private final Context cx;
+    private final Scriptable scope;
+
+    public Rhino() {
+        this.cx = Context.enter();
+        this.scope = cx.initStandardObjects();
+    }
+
+    public String execute(String script) {
+        String result = cx.evaluateString(scope, script, "<cmd>", 1, null);
+        return String.valueOf(result);
+    }
+
+    // call exist when deallocated
+    protected void finalize() throws Throwable {
+        Context.exit();
+        super.finalize();
+    }
+
+}
